fe7f4b3758280361cd94c10efdfbe0dbc2f6f5de
Ayush Kiledar <kiledar.ay@northeastern.edu>
2024-02-07 15:19:40 -0500

    workflow created

---
+++ src/main/java/com/example/cloudassignment01/Cloudassignment01Application.java
@@ -0,0 +1,18 @@
+package com.example.cloudassignment01;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cloudassignment01Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cloudassignment01Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
---
+++ src/test/java/com/example/cloudassignment01/Cloudassignment01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudassignment01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cloudassignment01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
