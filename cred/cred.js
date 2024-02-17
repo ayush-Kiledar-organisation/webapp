53de7029acfd93e7d5b1e33b61d90c37273bfee7
Ayush-neu23 <144970097+Ayush-neu23@users.noreply.github.com>
2024-02-17 01:45:28 -0500

    ass4 setup (#24)
    * assignment 4 setup
    
    * ass4 setup

--- cred/cred.js
+++ cred/cred.js
@@ -4,12 +4,14 @@ const User = require('../model/User');
 
 
 const credentials = new Sequelize(
+
     process.env.MYSQL_DATABASENAME,
     process.env.MYSQL_USERNAME, 
     process.env.MYSQL_ROOT_PASSWORD,{
     host: 'localhost',
     dialect: 'mysql',
     logging: false
+
     });
 
 const Schema = credentials.define('user',User);
--- test/apicheck.test.js
+++ test/apicheck.test.js
@@ -1,17 +1,25 @@
 const request = require('supertest');
-
-
+const app = require('../server');
 const generateMail = require('./mailgen');
+const { credentials } = require('../cred/cred');
 
 
 // Integration testing
 
 let email = generateMail();
-console.log(email)
+console.log(email);
+
+beforeAll(async()=>{
+  try {
+      await sequelize.sync({});
+      console.log('Db connected');
+    } catch (error) {
+      console.log('Error:', error);
+    }
+});
 
 // test for create and get user
 
-try{
   describe("TEST 1: Create user and get the user",()=>{
     test("creating an account", async()=>{
       const user_obj =
@@ -24,10 +32,12 @@ try{
        const resCreateUser = await request(app).post("/v1/user/self").send(user_obj);
   
        await expect(resCreateUser.statusCode).toBe(201);
+       console.log("Create status=> ",resCreateUser.statusCode)
   
        const token = Buffer.from(`${user_obj.email}:${user_obj.password}`).toString('base64');
   
         const resGetUser = await request(app).get("/v1/user/self").set("Authorization", `Basic ${token}`);
+        console.log("Get status1 => ",resGetUser.statusCode)
   
         expect (resGetUser.statusCode).toBe(200);
     });
@@ -57,60 +67,14 @@ try{
           expect(resGetUser.statusCode).toBe(200);
       });
   });  
-}
 
-catch(err){
-  console.log(err);
-}
-describe("TEST 1: Create user and get the user",()=>{
-  test("creating an account", async()=>{
-    const user_obj =
-      {
-        "email": email,
-      "firstName": "Fname",
-      "lastName": "Lname",
-      "password": "Testing@234",
-     };
-     const resCreateUser = await request(app).post("/v1/user/self").send(user_obj);
-
-     await expect(resCreateUser.statusCode).toBe(201);
-
-     const token = Buffer.from(`${user_obj.email}:${user_obj.password}`).toString('base64');
-
-      const resGetUser = await request(app).get("/v1/user/self").set("Authorization", `Basic ${token}`);
-
-      expect (resGetUser.statusCode).toBe(200);
-  });
+afterAll(async() => {
+  try {
+    await credentials.close();
+    console.log('Database connection closed');
+  } catch (error) {
+    console.error('Error closing:', error);
+  }
 });
 
-// test for update and get user
-describe("TEST 2: Update User and get the user",()=>{
-    test("Updating an existing user", async()=>{
-        const user_obj=
-            {
-                "firstName": "New fname",
-                "lastName": "New lname",
-                "password": "NewTesting@234",
-            };
 
-        const password ='Testing@234';
-
-        const token = Buffer.from(`${email}:${password}`).toString('base64');
-        const resUpdateUser = await request(app).put("/v1/user/self").set("Authorization", `Basic ${token}`).send(user_obj);
-        
-        await expect(resUpdateUser.statusCode).toBe(204);
-
-        const newToken = Buffer.from(`${email}:${user_obj.password}`).toString('base64');
-
-        const resGetUser = await request(app).get("/v1/user/self").set("Authorization", `Basic ${newToken}`);
-
-        expect(resGetUser.statusCode).toBe(200);
-    });
-});
-
-}
-catch(err){
-  console.log(err)
-}
-
-// test complete and succesful
