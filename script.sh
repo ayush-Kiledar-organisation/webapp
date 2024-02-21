353c7d79c952a36fe29f85a4828075da2d01ff09
Ayush Kiledar <kiledar.ay@northeastern.edu>
2024-02-21 12:14:07 -0500

    commit

--- script.sh
+++ script.sh
@@ -24,15 +24,14 @@ sudo yum install unzip
 
 sudo -u csye6225 bash
 
+sudo chown -R csye6225:csye6225 /opt/csye6225
+sudo chmod -R 750  /opt/csye6225
+
 sudo cp -r  webapp.zip /opt/csye6225
-
-
-# sudo chown -R csye6225:csye6225 /opt/csye6225/webapp
-# sudo chmod -R 750  /opt/csye6225/webapp
 
 cd /opt/csye6225
 sudo unzip webapp.zip
 
 cd webapp
-sudo npm install
+sudo npm install --build-from-source
 
