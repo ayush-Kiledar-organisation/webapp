477e9a89bb2033366a6c15305ec5bc2d322d8716
Ayush-neu23 <144970097+Ayush-neu23@users.noreply.github.com>
2024-02-21 13:11:53 -0500

    Pk2 (#53)
    * packer files and flow
    
    * rm file prov
    
    * added group sh
    
    * changed yml2
    
    * changed yml2
    
    * script
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit

--- script.sh
+++ script.sh
@@ -29,6 +29,12 @@ sudo -u csye6225 bash
 sudo chown -R csye6225:csye6225 /opt/csye6225
 sudo chmod -R 750  /opt/csye6225
 
+
+sudo -u csye6225 bash
+
+sudo chmod o+rx /opt/csye6225
+
+
 sudo cp -r  webapp.zip /opt/csye6225
 
 
