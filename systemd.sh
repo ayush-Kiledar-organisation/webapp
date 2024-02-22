0e26d58cff95999ac7524eadafc4345ffef4f268
Ayush-neu23 <144970097+Ayush-neu23@users.noreply.github.com>
2024-02-22 16:46:55 -0500

    Update systemd.sh

--- systemd.sh
+++ systemd.sh
@@ -6,7 +6,8 @@ ls -la /opt/csye6225
 
 echo "manage permissions for new user"
 sudo chown -R csye6225:csye6225 /opt/csye6225/webapp
-sudo chmod -R 777  /opt/csye6225/webapp
+sudo mkdir /var/log/csye6225
+sudo chown csye6225:csye6225 /var/log/csye6225
 
 echo "list all files"
 
@@ -24,7 +25,10 @@ ls -al
 
 echo "systemd setting"
 
+
 sudo cp csye6225.service /etc/systemd/system/
+sudo chown csye6225:csye6225 /etc/systemd/system/csye6225.service
 sudo systemctl daemon-reload
+sudo systemctl start csye6225
 sudo systemctl enable csye6225
 
