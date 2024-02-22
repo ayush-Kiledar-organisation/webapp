b57636512362fe959f33add8a74b88ad52df3e85
Ayush-neu23 <144970097+Ayush-neu23@users.noreply.github.com>
2024-02-21 20:54:18 -0500

    Pk8 (#63)
    * packer files and flow
    
    * rm file prov
    
    * added group sh
    
    * changed yml2
    
    * changed yml2
    
    * script
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit

--- systemd.sh
+++ systemd.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
-echo "get the home directory of user"
-echo ~csye6225
 
-echo "display permissions of user directory"
+echo "list all files"
 ls -la /opt/csye6225
 
-echo "change permissions of webapp"
+
+echo "manage permissions for new user"
 sudo chown -R csye6225:csye6225 /opt/csye6225/webapp
 sudo chmod -R 777  /opt/csye6225/webapp
+
+echo "list all files"
+
 ls -la /opt/csye6225
 
+echo "change user bash"
 sudo -u csye6225 bash
 
 
+echo "change dir"
 cd /opt/csye6225/webapp
 
-echo "checking files for error"
+echo "checking files"
 ls -al
+
+echo "systemd setting"
 
 sudo cp csye6225.service /etc/systemd/system/
 sudo systemctl daemon-reload
