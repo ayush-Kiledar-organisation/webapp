94eb249915d1ce2067b0a0fe6e7aa580fe8198cb
Ayush Kiledar <kiledar.ay@northeastern.edu>
2024-03-18 23:38:24 -0400

    auth logs

--- security/auth.js
+++ security/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const User = require('../cred/cred').Schema;
 const bcrypt = require('bcryptjs');
+const logger = require('../logger');
 
 const authenticated = async (email, password) => {
 
@@ -22,9 +23,10 @@ const authenticated = async (email, password) => {
 const basicAuth = async(req, res, next) => {
 
     if (!req.get('Authorization')) {
-        const err = new Error('Not Authenticated');
+        // const err = new Error('Not Authenticated');
+        logger.error('Not Authenticated');
         res.status(401).json({ error: "Not Authenticated" });
-        return next(err);
+        return;
       }
 
       var data = Buffer.from(req.get('Authorization').split(' ')[1], 'base64');
@@ -32,9 +34,10 @@ const basicAuth = async(req, res, next) => {
       
     const authenticate = await authenticated(email, password);
     if (!authenticate) {
-    const err = new Error('Invalid Credentials');
-    res.status(401).json({ error: err.message });
-    return next(err);
+    // const err = new Error('Invalid Credentials');
+    logger.fatal('Invalid Credentials');
+    res.status(401).json({ error: 'Invalid Credentials' });
+    return;
     }
     next();
 
