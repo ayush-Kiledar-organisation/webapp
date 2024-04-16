375d01dc6bd27700b772afe4dbd651d1020f78e5
Ayush Kiledar <kiledar.ay@northeastern.edu>
2024-04-16 18:47:35 -0400

    commit

--- routes/user.route.js
+++ routes/user.route.js
@@ -5,10 +5,10 @@ const basicAuth = require('../security/auth');
 const { getUser, createUser, updateUser } = require('../controllers/user.controller');
 const User = require('../cred/cred').Schema;
 
-router.get('/v1/user/self',basicAuth, getUser);
+router.get('/v10/user/self',basicAuth, getUser);
 
-router.post('/v1/user/self', createUser);
+router.post('/v10/user/self', createUser);
 
-router.put('/v1/user/self', basicAuth, updateUser);
+router.put('/v10/user/self', basicAuth, updateUser);
 
 module.exports = router;
